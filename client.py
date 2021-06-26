9d1efef1d3b40c318a0eb75c10cbda898de043a2
azizmap <azizmpratama@gmail.com>
2021-06-26 10:25:47 +0700

    major feature update

--- client.py
+++ client.py
@@ -2,26 +2,71 @@
 
 # import xmlrpc bagian client saja
 import xmlrpc.client
+import os
 
 # buat stub (proxy) untuk client
-s = xmlrpc.client.ServerProxy('http://54.151.246.141:5000')
+s = xmlrpc.client.ServerProxy('http://54.151.246.141:8080')
 
 # lakukan pemanggilan fungsi vote("nama_kandidat") yang ada di server
-print("Masukkan NIK :")
-nik = input()
-print("Masukkan nama terduga covid :")
-terduga = input()
-print("Masukkan alamat :")
-alamat = input()
-print("Gejalan yang dirasakan terduga:")
-keluhan = input()
-s.ceknik(nik, terduga, alamat, keluhan)
+# status = True
+while True:
+  print("Silahkan pilih menu :")
+  print("1. Lapor Covid-19")
+  print("2. Cek Pengantaran")
+  print("3. Keluar")
+  print("Silahkan pilih menu (1 atau 2) :")
+  pil = int(input())
+  print()
 
-print()
-print()
-print()
+  if(pil == 1):
+    print("Masukkan NIK :")
+    nik = input()
+    print("Masukkan nama terduga covid :")
+    terduga = input()
+    print("Masukkan alamat :")
+    alamat = input()
+    print("Gejala yang dirasakan terduga:")
+    keluhan = input()
+    s.laporan(nik, terduga, alamat, keluhan)
+
+    os.system('cls')
+    hasil = s.query()
+    print('=======================PERHATIAN !!!!!=======================')
+    if isinstance(hasil, list):
+      print('Id Jemputan : ', hasil[0])
+      print('Dijemput pukul : ', hasil[1])
+      print('Tim Penjemput : ', hasil[2])
+      print('Anggota 1 : ', hasil[3])
+      print('Anggota 2 : ', hasil[4])
+      print('Anggota 3 : ', hasil[5])
+      print('')
+    else:
+      print(hasil)
+      print('')
+  elif(pil == 2):
+    print("masukkan id jemputan")
+    id = int(input())
+    hasil = s.cek_jemput(id)
+
+    os.system('cls')
+    print('=======================PERHATIAN !!!!!=======================')
+    if isinstance(hasil, list):
+      print('Id Jemputan : ', hasil[0])
+      print('Dijemput pukul : ', hasil[1])
+      print('Tim Penjemput : ', hasil[2])
+      print('Anggota 1 : ', hasil[3])
+      print('Anggota 2 : ', hasil[4])
+      print('Anggota 3 : ', hasil[5])
+      print('')
+    else:
+      print(hasil)
+    print('')
+    # panggil fungsi cek jemputan
+  elif(pil == 3):
+    os.system('cls')
+    break
+  else:
+    os.system('cls')
+
 # lakukan pemanggilan fungsi querry() untuk mengetahui hasil persentase dari masing-masing kandidat
-hasil = s.query()
-print('=======================PERHATIAN !!!!!=======================')
-print(hasil)
 # lakukan pemanggilan fungsi lain terserah Anda
--- server.py
+++ server.py
@@ -2,6 +2,7 @@
 
 # import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCServer
+from datetime import datetime, timedelta
 import pandas as pd
 
 # import SimpleXMLRPCRequestHandler
@@ -13,28 +14,26 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
 # Buat server
-#"127.0.0.1", 8008
-with SimpleXMLRPCServer(("0.0.0.0", 5000),
+#"0.0.0.0", 8008
+with SimpleXMLRPCServer(("0.0.0.0", 8080),
         requestHandler=RequestHandler, allow_none=True) as server:
     server.register_introspection_functions()
 
     #server.register_function(pow)
     # buat data struktur dictionary untuk menampung nama_kandidat dan hasil voting
     data_rakyat = pd.read_csv('data_rakyat.csv')
+    data_penjemput = pd.read_csv('anggota_jemput.csv')
     data_laporan = []
     data_jemputan = []
     data = {}
     
-    # kode setelah ini adalah critical section, menambahkan vote tidak boeh terjadi race condition
-    # siapkan lock
     lock = threading.Lock()
 
-    # def post(x) :
-    #     print(x)
-    #     lock.release()
+    #status untuk berhasil add laporan atau tidak
     status = 0
+    tim = 1
     #  buat fungsi bernama vote_candidate()
-    def cek_nik(nik, terduga, alamat, gejala):
+    def add_laporan(nik, terduga, alamat, gejala):
         
         # critical section dimulai harus dilock
         lock.acquire()
@@ -43,8 +42,6 @@ with SimpleXMLRPCServer(("0.0.0.0", 5000),
         data = data_rakyat[data_rakyat['NIK'] == int(nik)]
         laporan = {}
         if len(data) > 0:
-            # nik = data['NIK']
-            # nama = data['Nama']
             laporan['nik'] = nik
             laporan['nama'] = data['Nama'][0]
             laporan['terduga'] = terduga
@@ -52,13 +49,13 @@ with SimpleXMLRPCServer(("0.0.0.0", 5000),
             laporan['gejala'] = gejala
             status = 1
             data_laporan.append(laporan)
-            # data_laporan.append([nik, nama, terduga, alamat, gejala])
+            
         # critical section berakhir, harus diunlock
         lock.release()
         
     
     # register fungsi vote_candidate() sebagai vote
-    server.register_function(cek_nik, 'ceknik')
+    server.register_function(add_laporan, 'laporan')
 
     # buat fungsi bernama querry_result
     def querry_result():
@@ -67,30 +64,43 @@ with SimpleXMLRPCServer(("0.0.0.0", 5000),
         print(data_laporan)
         lock.acquire()
         global status
+        global tim
         if(status != 0):
             status = 0
-            lock.release()
-            data_jemputan.append('Anda akan dijemput pada ..... oleh .....')
+
+            jam_jemput = datetime.now() + timedelta(hours = 1)
+            jam_jemput = jam_jemput.strftime("%H:%M:%S")
+            id = len(data_jemputan)
+            tim_penjemput = tim
+            penjemput = data_penjemput[data_penjemput['id_tim']==tim_penjemput]
+            data_jemputan.append([id, jam_jemput, tim_penjemput])
+            
+            tim += 1
+            if (tim > len(data_penjemput)):
+                tim = 1
+
+            lock.release()            
             print('================================Data JEMPUTAN================================')
             print(data_jemputan)
-            return ('Anda akan dijemput pada ..... oleh .....')
+            return([id, jam_jemput, tim_penjemput, penjemput['Anggota1'].values[0], penjemput['Anggota2'].values[0], penjemput['Anggota3'].values[0]])
+            # return ('Nomor jemputan : ',id,', pukul ',jam_jemput,' oleh ',data_jemputan[id][2])
         else:
             lock.release()
             return ('NIK tidak terdaftar')
-        # hitung total vote yang ada
-        # total_vote = sum(nama_kandidat.values())
-        # hasil = []
-        # # hitung hasil persentase masing-masing kandidat
-        # for keys in nama_kandidat:
-        #     hasil.append([keys, nama_kandidat[keys] / total_vote * 100])
-        
-        # # critical section berakhir
-        # lock.release()
-        # print(data_laporan)
-        # return ('kamu dijemput sama kakak apink jam 2')
-    # register querry_result sebagai querry
     server.register_function(querry_result, 'query')
 
+    def query_cek(id):
+        lock.acquire()
+        if (id < 0 or id >= len(data_jemputan)):
+            lock.release()
+            return('Data id tidak ada')
+        
+        tim_penjemput = data_jemputan[id][2]
+        penjemput = data_penjemput[data_penjemput['id_tim']==tim_penjemput]
+        lock.release()
+        # return ('Nomor jemputan : ',data_jemputan[id][0],', pukul ',data_jemputan[id][1],' oleh ',data_jemputan[id][2])
+        return([data_jemputan[id][0], data_jemputan[id][1], tim_penjemput, penjemput['Anggota1'].values[0], penjemput['Anggota2'].values[0], penjemput['Anggota3'].values[0]])
+    server.register_function(query_cek, 'cek_jemput')
 
     print ("Server covid berjalan...")
     # Jalankan server
